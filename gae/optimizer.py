34a93c58c1619ee512a4fb5b9515b1aa449a24e0
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-05 12:13:30 +0800

    gae check

--- gae/optimizer.py
+++ gae/optimizer.py
@@ -12,4 +12,8 @@ def loss_function(preds, labels, mu, logvar, n_nodes, norm, pos_weight):
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 / n_nodes * torch.mean(torch.sum(
         1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1))
+
+
+    print (cost, KLD)
+
     return cost + KLD
