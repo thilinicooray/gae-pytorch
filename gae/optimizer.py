a451e09ebfc3e10247dd824d4320e30dc2de3ab1
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-05 12:49:26 +0800

    gae check

--- gae/optimizer.py
+++ gae/optimizer.py
@@ -5,17 +5,25 @@ import torch.nn.functional as F
 
 def loss_function(preds, labels, mu, logvar, grouped_mu, grouped_logvar, n_nodes, norm, pos_weight):
 
-    cost = F.binary_cross_entropy_with_logits(preds, labels)
+    cost = norm* F.binary_cross_entropy_with_logits(preds, labels, pos_weight=pos_weight)
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 / n_nodes * torch.mean(torch.sum(
-        1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1))
+    '''KLD = -0.5 / n_nodes * torch.mean(torch.sum(
+        1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1))'''
 
-    KLD_g = -0.5 / n_nodes * torch.mean(torch.sum(
-        1 + 2 * grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp().pow(2), 1))
+    KLD = torch.mean(
+        - 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    )
+
+    '''KLD_g = -0.5 / n_nodes * torch.mean(torch.sum(
+        1 + 2 * grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp().pow(2), 1))'''
+
+    KLD_g = torch.mean(
+        - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
+    )
 
 
     #print (cost, KLD, KLD_g)
